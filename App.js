1245d16371cf46d455df72d1ba9bdb2c4517d1a4
arshshaik324@gmail.com <arshshaik324@gmail.com>
2023-07-27 01:07:53 +0530

    React Assessment

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AccordionList from './AccordionList';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Accordion List</h1>
+      <AccordionList />
+    </div>
+  );
+};
+
+export default App;
